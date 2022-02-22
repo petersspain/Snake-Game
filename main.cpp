ba7dd106b63cbef3034c924b49c2bcb811f119d8
Nikita Danilov <danilov.nikita.inform@mail.ru>
2022-02-22 19:22:33 +0300

    first window and shaders

--- main.cpp
+++ main.cpp
@@ -1,8 +1,68 @@
 #include <iostream>
 
-using namespace std;
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
 
 int main() {
-	cout << "Hello, world!" << endl;
+	// GLFW initialisation
+	if (!glfwInit()) {
+		std::cerr << "GLFW initialisation failed" << std::endl;
+		glfwTerminate();
+		return 1;
+	}
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "SnakeGame", 0, 0);
+	if (window == NULL) {
+		std::cerr << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return 2;
+	}
+	glfwMakeContextCurrent(window);
+
+	// GLEW initialisation
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK) {
+		std::cerr << "GLEW initialisation failed!" << std::endl;
+		glfwDestroyWindow(window);
+		glfwTerminate();
+		return 3;
+	}
+
+	glViewport(0, 0, WIDTH, HEIGHT);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	while (!glfwWindowShouldClose(window)) {
+		// input
+		processInput(window);
+
+		// rendering commands
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// check and call events and swap buffers
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
 	return 0;
 }
